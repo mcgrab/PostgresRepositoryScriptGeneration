b94e862783e869994d17c1ac50529a753e452c63
Yury Kozhanovskiy <mcgrab095@gmail.com>
2019-02-24 14:14:04 +0300

    First implementation

---
+++ ScriptGeneration/ScriptGeneration/Attributes/CanUpdateAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptGeneration.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property |
+                    AttributeTargets.Field, AllowMultiple = false)]
+    public class CanUpdateAttribute:Attribute
+    {
+    }
+}
---
+++ ScriptGeneration/ScriptGeneration/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptGeneration.Repository
+{
+    public interface ITestRepository<T> where T : class
+    {
+        void Add(T item);
+        void Remove(long id);
+        void Update(T item, dynamic where);
+        IEnumerable<T> GetMany(dynamic where);
+        T GetOne(dynamic where);
+        IEnumerable<T> FindAll(int offset = 0, int count = 0, string order = "ASC");
+    }
+}
